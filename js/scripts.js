ed4153e580036da0c56846aee3a7214f58d4a1db
Calvin Cushman <calvincushman@gmail.com>
2017-01-27 13:36:54 -0800

    add sauce cheese and topping selection fields

--- js/scripts.js
+++ js/scripts.js
@@ -3,19 +3,33 @@ $(document).ready(function() {
   var size;
   var crust;
   var sauce;
-  var toppings;
-
+  var selectedToppings;
+  var toppings = [];
+  var cheese = [];
   //constructor
   function Pizza(size, crust, sauce, toppings) {
     this.size =size;
     this.crust = crust;
     this.sauce = sauce;
     this.toppings = toppings;
+    this.cheese = cheese;
   }
   $("#pizzaToOrder").click(function(event) {
-    size =$("#sizeSelection").val();
-    crust =$("input:radio[name=crust]:checked").val();
-    var pizza = new Pizza(size, crust, sauce, toppings);
+    size = $("#sizeSelection").val();
+    crust = $("input:radio[name=crust]:checked").val();
+    sauce = $("input:radio[name=sauce]:checked").val();
+
+    $("input:checkbox[name=toppings]:checked").each(function() {
+      var selectedToppings = $(this).val();
+      toppings.push(selectedToppings);
+
+    });
+    $("input:checkbox[name=cheese]:checked").each(function() {
+      var selectedCheese = $(this).val();
+      cheese.push(selectedCheese);
+
+    });
+    var pizza = new Pizza(size, crust, sauce, cheese, toppings);
     console.log(pizza);
     event.preventDefault();
   });
