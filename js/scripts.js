7384142daebde2d79cc68813bbcdff579b21ad2f
Calvin Cushman <calvincushman@gmail.com>
2017-01-27 11:47:57 -0800

    add size and crust selection

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,29 @@
 $(document).ready(function() {
   //global variables
-  var size = 0;
-  var toppings = 0;
+  var size;
+  var crust;
+  var sauce;
+  var toppings;
 
   //constructor
-  function Pizza(size, crust, sauce, topping) {
+  function Pizza(size, crust, sauce, toppings) {
+    this.size =size;
     this.crust = crust;
     this.sauce = sauce;
-    this.size =size;
+    this.toppings = toppings;
   }
-  $("#pizzaToOrder").click(function(pizza) {
-    $("form#sizeSelection").click(function(event) {
-      var pizzSize = $("#sizeSelection").val();
-    $("form#crustSelection")
-      var crust =$("#crust").val();
-
-      var pizza = new Pizza(size);
-    })
-    var pizza = new Pizza(pizzaSize);
+  $("#pizzaToOrder").click(function(event) {
+    size =$("#sizeSelection").val();
+    crust =$("input:radio[name=crust]:checked").val();
+    var pizza = new Pizza(size, crust, sauce, toppings);
     console.log(pizza);
     event.preventDefault();
   });
 
-
 });
+
+
+
+
+
  //pizza order function
