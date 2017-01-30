c6bee8cd894ddcf74d5d748674cdebd2db1ffbeb
Calvin Cushman <calvincushman@gmail.com>
2017-01-30 10:45:23 -0800

    refactor cheese and topping logic

---
+++ README.md
@@ -0,0 +1,55 @@
+## Pizza Pie
+
+### Epicodus Week 4 independant code review
+
+### Calvin Cushman on 1/27/2017
+
+#### Description: takes an order from a customer for a pizza.
+
+#### Specs
+
+* Program will take information from a customer.
+
+* Program will combine the elements into a pizza.
+
+* Program will take into account the number of toppings and cheeses that a customer orders.
+
+* Program will take into account the size of pizza that a customer orders.
+
+* Program will use the number of toppings, cheese, and the size to determine a price for the pizza.
+
+* Program will return a price to the user along with a detailed description of their pizza.
+
+* Program will store the ordered pizza and form will clear for additional pizzas. 
+
+### Technologies Used
+
+* HTML, CSS, JavaScript
+* jQuery
+* Bootstrap
+* git
+
+### setup
+
+* Clone repository
+* Open index.html
+
+### See on GitHub.io
+
+
+
+### Known Issues
+
+* does not yet fully calculate user's total
+
+### Support
+
+* Calvincushman@gmail.com
+
+### Copyright
+
+* 2017 Calvin Cushman
+
+### license
+
+* MIT
--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,7 @@
 $(document).ready(function() {
   //global variables
-  var size;
-  var crust;
-  var sauce;
-  var selectedToppings;
-  var toppings = [];
-  var cheese = [];
-  var price;
+  var pizzaToOrder
+
 
   //pizza constructor
   function Pizza(size, crust, sauce, toppings) {
@@ -17,44 +12,71 @@ $(document).ready(function() {
     this.cheese = cheese;
   }
 
-  //pizza building process
-  $("#pizzaToOrder").click(function(event) {
-    size = $("#sizeSelection").val();
-    crust = $("input:radio[name=crust]:checked").val();
-    sauce = $("input:radio[name=sauce]:checked").val();
-
-    $("input:checkbox[name=toppings]:checked").each(function() {
-      var selectedToppings = $(this).val();
-      toppings.push(selectedToppings);
-
-    });
-    $("input:checkbox[name=cheese]:checked").each(function() {
-      var selectedCheese = $(this).val();
-      cheese.push(selectedCheese);
-
-    });
-
-    var pizza = new Pizza(size, crust, sauce, cheese, toppings);
-    console.log(pizza);
-
-    var toppingPrice = 0;
-    var cheesePrice = 0;
+  $("input:checkbox[name=toppings]:checked").each(function() {
+    var selectedToppings = $(this).val();
+    toppings.push(selectedToppings);
 
     toppings.forEach(function(topping) {
     toppingPrice += 1;
     console.log(toppingPrice);
+
     });
+  });
+
+  $("input:checkbox[name=cheese]:checked").each(function() {
+    var selectedCheese = $(this).val();
+    cheese.push(selectedCheese);
+
     cheese.forEach(function(cheese) {
     cheesePrice += 1;
     console.log(cheesePrice);
 
-    event.preventDefault();
     });
   });
 
-  //if (size === tiny) {
-    //$()
-  //}
+  function Pizzaprice(sizePrice, cheesePrice, toppingPrice) {
+    this.sizePrice =sizePrice;
+    this.cheesePrice = cheesePrice;
+    this.toppingPrice = toppingPrice;
+
+  if (size = "Tiny") {
+    size === 7;
+  }
+
+  //pizza building process
+  $("#pizzaToOrder").click(function(event) {
+    var crust;
+    var sauce;
+    var toppings = [];
+    var cheese = [];
+    var size;
+
+    size = $("#sizeSelection").val();
+    crust = $("input:radio[name=crust]:checked").val();
+    sauce = $("input:radio[name=sauce]:checked").val();
+
+    var pizza = new Pizza(size, crust, sauce, cheese, toppings);
+    console.log(pizza);
+
+    toppings.forEach(function(topping) {
+    toppingPrice += 1;
+    console.log(toppingPrice);
+
+    });
+
+    cheese.forEach(function(cheese) {
+    cheesePrice += 1;
+    console.log(cheesePrice);
+
+    });
+
+    size.
+
+    event.preventDefault();
+  });
+
+  return pizza;
+
 
 
 });
