331524e1ec098bc778a79069d5184978ee9b12d4
Calvin Cushman <calvincushman@gmail.com>
2017-01-30 16:32:45 -0800

    separate present business and user logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,69 +1,52 @@
-$(document).ready(function() {
-  //global variables
-  var pizzaToOrder
-
-
   //pizza constructor
-  function Pizza(size, crust, sauce, toppings, cheese) {
-    this.size =size;
-    this.crust = crust;
-    this.sauce = sauce;
-    this.toppings = toppings;
-    this.cheese = cheese;
-  };
+function Pizza(size, crust, sauce, toppings, cheese) {
+  this.size =size;
+  this.crust = crust;
+  this.sauce = sauce;
+  this.toppings = toppings;
+  this.cheese = cheese;
+};
 
-  function Pizzaprice(sizePrice, cheesePrice, toppingPrice) {
-    this.sizePrice =sizePrice;
-    this.cheesePrice = cheesePrice;
-    this.toppingPrice = toppingPrice;
-  };
+function SelectElement(valueToSelect) {
+  var element = document.getelementById('sizeSelection')
+  element.value = valueToSelect;
+};
 
-  //pizza building process
-  $("#pizzaToOrder").click(function(event) {
+
+
+$(document).ready(function() {
+  $("#add-pizza").click(function(event) {
     var crust;
     var sauce;
     var toppings = [];
-    var toppingPrice;
     var cheese = [];
-    var cheesePrice;
     var size;
-
-    var pizza = new Pizza(size, crust, sauce, toppings, cheese);
 
     size = $("#sizeSelection").val();
     crust = $("input:radio[name=crust]:checked").val();
     sauce = $("input:radio[name=sauce]:checked").val();
 
     $("input:checkbox[name=toppings]:checked").each(function() {
-      var selectedToppings = $(this).val();
-      toppings.push(selectedToppings);
-
-      toppings.forEach(function(topping) {
-      toppingPrice += 1;
-      console.log(toppingPrice);
-
-      });
+      var selectedTopping = $(this).val();
+      toppings.push(selectedTopping);
     });
+
+    var toppingsPrice = parseInt($(toppings.length)val());
 
     $("input:checkbox[name=cheese]:checked").each(function() {
       var selectedCheese = $(this).val();
       cheese.push(selectedCheese);
-
-      cheese.forEach(function(cheese) {
-      cheesePrice += 1;
-      console.log(cheesePrice);
-
-      });
     });
+
+    var cheesePrice = parseInt($(cheese.length).val());
+    var sizePrice = parseInt($(size).val());
 
     var pizza = new Pizza(size, crust, sauce, toppings, cheese);
     console.log(pizza);
-
-    return pizza;
-    $("#order-output").text(pizza);
-
+    console.log(toppingsPrice);
+    console.log(cheesePrice);
+    console.log(sizePrice);
     event.preventDefault();
-  });
 
-  //$("#order-output").text(result);
+  });
 });
